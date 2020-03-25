e9e49ff9455efd04af3bf48e0064ba6227c84e7d
DevOnLin <reedthink@qq.com>
2020-03-25 18:39:24 +0800

    面试题64

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Leetcode刷题记录
 https://leetcode-cn.com/problemset/all/
 
 ## 2020/3/25 
-- 1道easy
+- easy: 1
+- meduim: 1
 
-残念：leetcode英文站很难登进去，只有用中文站了，难受啊
+残念：leetcode英文站很难登进去，只有用中文站了，难受啊  
 不能在本地调试，难受啊
---
+++ 面试题64.cpp
@@ -0,0 +1,10 @@
+//面试题64.cpp
+// https://leetcode-cn.com/problems/qiu-12n-lcof/
+class Solution {
+public:
+    int sumNums(int n) {
+        // int ans=0;/
+        n&&(n=sumNums(n-1)+n);
+        return n;
+    }
+};
